1461f46a679ca499b9a65f991fd1d887b8867413
Riley Butler <thebutler42@gmail.com>
2026-09-07 22:32:57 -0700

    block parse complete

--- src/blockparse.py
+++ src/blockparse.py
@@ -14,23 +14,29 @@ def markdown_to_blocks(markdown: str):
 
     return [item.strip() for item in block_chain if item.strip()]
 
+
+def determine_if_ordered_list(md_block: str):
+    list_nums = re.findall(r'^([0-9][0-9]|[0-9])\. [\s\S]*$', md_block, flags = re.MULTILINE)
+    nums_ordered = True
+    for i in range(len(list_nums)):
+        if int(list_nums[i]) != (i + 1):
+            nums_ordered = False
+    return nums_ordered
+
+
+
 def block_to_block_type(md_block: str):
-    if re.fullmatch(r'^#{1,6}\s.*$',md_block):
+    if re.fullmatch(r'^[0-9]{1,6}\s.*$',md_block):
         return BlockType.heading
     elif re.fullmatch(r'^```\n[\s\S]*```$',md_block):
         return BlockType.code
-    elif re.fullmatch(r'^>.*$',md_block, flag=re.MULTILINE):
+    elif re.fullmatch(r'^>[\s\S]*$',md_block, flags=re.MULTILINE):
         return BlockType.quote
-    elif re.fullmatch(r'^- .*$', md_block, flag=re.MULTILINE):
+    elif re.fullmatch(r'^- [\s\S]*$', md_block, flags=re.MULTILINE):
         return BlockType.unordered_list
-    list_nums = re.findall(r'^([0-9][0-9]|[0-9])\. .*$', md_block, flag=re.MULTILINE)
-    nums_ordered = True
-        for i in range(len(list_nums)):
-            if list_nums[i] != i + 1:
-            nums_ordered = False
-
-
-    elif re.fullmatch(r'^([0-9][0-9]|[0-9])\. .*$', md_block, flag = re.MULTILINE) and nums_ordered:
+    elif re.fullmatch(r'^([0-9][0-9]|[0-9])\. [\s\S]*$', md_block, flags = re.MULTILINE) and (
+        determine_if_ordered_list(md_block)
+        ):
         return BlockType.ordered_list
     else:
         return BlockType.paragraph
--- src/test_blockparse.py
+++ src/test_blockparse.py
@@ -1,5 +1,5 @@
 import unittest
-from blockparse import markdown_to_blocks
+from blockparse import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlockParse(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -51,3 +51,59 @@ This is the same paragraph on a new line
             markdown_to_blocks(md),
             ["this just a line"]
         )
+
+    def test_block_type_check(self):
+        self.assertEqual(
+            BlockType.paragraph,
+            block_to_block_type("skfjla")
+
+        )
+        self.assertEqual(
+            BlockType.heading,
+            block_to_block_type("124 headings")
+            
+        )
+        self.assertEqual(
+            BlockType.code,
+            block_to_block_type("""```
+            code block text you know
+            this is still a code block
+            > ohkk
+            21 sldkf
+            ```""")
+
+        )
+
+        quote = """> quote block
+>these are quote
+>still a quote"""
+        self.assertEqual(
+            BlockType.quote,
+            block_to_block_type(quote)
+            
+        )
+        self.assertEqual(
+            BlockType.unordered_list,
+            block_to_block_type(
+                """- list item,
+- next list item,
+- another list item"""
+            )
+        )
+        self.assertEqual(
+            BlockType.ordered_list,
+            block_to_block_type(
+                """1. numberone
+2. number two"""
+            )
+        )
+        self.assertNotEqual(
+            BlockType.ordered_list,
+            block_to_block_type(
+                """2. numbertwo,
+3. out of order"""
+            )
+        )
+        
+
+
